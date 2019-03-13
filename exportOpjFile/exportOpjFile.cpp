c41cec4bacd0d1276dd71b6e096d546ca47342f0
Amaury Guillermin <amaury.guillermin@ec-lyon.fr>
2019-03-13 15:26:43 +0100

    Add writting file

--- exportOpjFile/exportOpjFile.cpp
+++ exportOpjFile/exportOpjFile.cpp
@@ -2,12 +2,19 @@
 //
 
 #include "OriginFile.h"
+
 #include <iostream>
+#include <fstream>
+#include <cstdint>
+#include <experimental/filesystem>
+
+namespace fs = std::experimental::filesystem::v1;
 
 int main(int argc, char *argv[])
 {
 	bool parseData = false;
-	char OpjExtension = '.opj';
+	string OpjExtension = ".opj";
+	bool endLine;
 
 	if (argc < 2) {
 		cout << "Usage: exportOpjFile.exe <file.opj>" << endl;
@@ -17,7 +24,16 @@ int main(int argc, char *argv[])
 	string input = argv[argc - 1];
 	
 	int folderNameIdx = input.find(OpjExtension);
-	string folder = input.substr(1, folderNameIdx);
+	string folder = input.substr(0, folderNameIdx);
+
+	const int dir_err = fs::create_directory(folder.c_str());
+	if (dir_err == -1 && !fs::exists(folder)) {
+		cout << "Error: impossible to create folder to save file" << endl;
+		return -1; 
+	}
+	else {
+		cout << "Initializing saving file in the folder: " << folder.c_str() << endl;
+	}
 
 	// Open File
 	OriginFile opj(input);
@@ -43,21 +59,36 @@ int main(int argc, char *argv[])
 		string name = excel.name;
 		string label = excel.label;
 
-		int FileNameIdx = label.find(EOL);
-		string FileName = label.substr(1, FileNameIdx);
+		int FileNameIdx = label.find("\n");
+		string FileName = label.substr(0, FileNameIdx-1);
 		
 		cout << "Excel " << (e + 1) << endl;
 		cout << " Name: " << name.c_str() << endl;
-		cout << " Label: " << label.c_str() << endl;
+		cout << " Label: " << FileName << endl;
 
 		vector<Origin::SpreadSheet> sheets = excel.sheets;
+
+		string filepath = folder + string("/") + FileName + OpjExtension;
+		cout << filepath << endl; 
+		ofstream outputFile(filepath.c_str(), ios::out);
+
+		outputFile << "Name: " << label << endl; 
+		outputFile << endl; 
+
+		if (outputFile) {
+			cout << "Exporting data in filename: " << filepath << endl;
+		}
+		else {
+			cout << "Error: Unable to create file to save data" << endl;
+			return -1;
+		}
 
 		for (int s = 0; s < sheets.size(); s++) {
 			Origin::SpreadSheet sheet = sheets[s];
 
-			cout << "Spreadsheet " << (s + 1) << endl;
-			cout << " Name: " << sheet.name.c_str() << endl;
-			cout << " Label: " << sheet.label.c_str() << endl;
+			cout << "  Spreadsheet " << (s + 1) << endl;
+			cout << "   Name: " << sheet.name.c_str() << endl;
+			cout << "   Label: " << sheet.label.c_str() << endl;
 
 			if (sheet.name == "Data") parseData = true; else parseData = false;
 			
@@ -69,44 +100,52 @@ int main(int argc, char *argv[])
 			for (int c = 0; c < columnsCount; c++) {
 				Origin::SpreadColumn column = columns[c];
 				cout << "	Column " << (c + 1) << " : " << column.name.c_str() << " / type : " << column.type << ", rows : " << sheet.maxRows << endl;
+			}
 
-				int dataCount = column.data.size();
+				
+			int dataCount = sheet.columns[0].data.size();
 
-				if (parseData) {
-					for (int d = 0; d < dataCount; d++) {
-						Origin::variant value(column.data[d]);
+			if (parseData) {
+				for (int d = 0; d < dataCount; d++) {
+					for (int col = 0; col < columnsCount; col++) {
+
+						string dataSeparator = "	";
+						if (col == columnsCount - 1) endLine = 1; else endLine = 0;
+						string sep = endLine ? "\n" : dataSeparator;
+
+						Origin::variant value(sheet.columns[col].data[d]);
 
 						switch (value.type()) {
 						case Origin::variant::V_DOUBLE: {
 							double v = value.as_double();
 
 							if (v != _ONAN) {
-								cout << v << "; ";
+								cout << v << ";" ;
+								outputFile << v << sep.c_str();
+
 							}
 							else {
 								cout << nan("NaN") << "; ";
+								outputFile << nan("NaN") << sep.c_str();
 							}
 
 							break;
 						}
 						case Origin::variant::V_STRING:
-							cout << value.as_string() << ",";
+							cout << value.as_string() << sep.c_str();
 							break;
 						default:
-							cout << nan("NaN") << ",";
+							cout << nan("NaN") << sep.c_str();
 							break;
 						}
 					}
-
-					cout << endl;
 				}
-
-				
 			}
 
-
-			
+			outputFile << endl;
 		}
+
+		outputFile.close();
 	}
 }
 // Exécuter le programme : Ctrl+F5 ou menu Déboguer > Exécuter sans débogage
